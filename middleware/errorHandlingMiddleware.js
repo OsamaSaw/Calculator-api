b15505f3b804507d1b2a10a699cc9c0a14b9d549
Osama <Osama@fitlov.com>
2023-12-28 05:00:34 +0200

    adding no params error handling

--- middleware/errorHandlingMiddleware.js
+++ middleware/errorHandlingMiddleware.js
@@ -6,17 +6,20 @@ const errorHandlingMiddleware = (req, res, next) => {
     const minAllowedValue = -10000000;
 
     let errorMessage = '';
-
-    if (!/^\-?\d+(\.\d+)?$/.test(num1) || !/^\-?\d+(\.\d+)?$/.test(num2)) {
-        errorMessage = 'Inputs must be valid numbers';
+    if (num1 === undefined || num2 === undefined) {
+        errorMessage = 'Missing required parameters: num1 and num2';
     } else {
-        let num1Converted = parseFloat(num1);
-        let num2Converted = parseFloat(num2);
+        if (!/^\-?\d+(\.\d+)?$/.test(num1) || !/^\-?\d+(\.\d+)?$/.test(num2)) {
+            errorMessage = 'Inputs must be valid numbers';
+        } else {
+            let num1Converted = parseFloat(num1);
+            let num2Converted = parseFloat(num2);
 
-        if (num1Converted > maxAllowedValue || num1Converted < minAllowedValue || num2Converted > maxAllowedValue || num2Converted < minAllowedValue) {
-            errorMessage = `Inputs must be between ${minAllowedValue} and ${maxAllowedValue}`;
-        } else if (req.path === '/divide' && num2Converted === 0) {
-            errorMessage = 'Cannot divide by zero';
+            if (num1Converted > maxAllowedValue || num1Converted < minAllowedValue || num2Converted > maxAllowedValue || num2Converted < minAllowedValue) {
+                errorMessage = `Inputs must be between ${minAllowedValue} and ${maxAllowedValue}`;
+            } else if (req.path === '/divide' && num2Converted === 0) {
+                errorMessage = 'Cannot divide by zero';
+            }
         }
     }
 
